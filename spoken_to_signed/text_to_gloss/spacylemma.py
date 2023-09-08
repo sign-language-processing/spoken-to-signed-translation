2ce14b81d1def6fc9eefc61c744c3bca7cb53909
Mathias Müller <mathias.mueller@uzh.ch>
2023-09-08 10:12:59 +0200

    spacylemma: more languages, more efficient

--- spoken_to_signed/text_to_gloss/spacylemma.py
+++ spoken_to_signed/text_to_gloss/spacylemma.py
@@ -4,7 +4,9 @@ from .common import load_spacy_model
 
 LANGUAGE_MODELS_SPACY = {
     "de": "de_core_news_lg",
-    "fr": "fr_core_news_lg"
+    "fr": "fr_core_news_lg",
+    "it": "it_core_news_lg",
+    "en": "en_core_web_lg",
 }
 
 
@@ -15,7 +17,9 @@ def text_to_gloss(text: str, language: str, ignore_punctuation: bool = True) -> 
 
     model_name = LANGUAGE_MODELS_SPACY[language]
 
-    spacy_model = load_spacy_model(model_name)
+    # disable unnecessary components to make lemmatization faster
+
+    spacy_model = load_spacy_model(model_name, disable=["parser", "ner"])
 
     doc = spacy_model(text)
 
