76222d4e821fdd700718b25c94775dc5b63b0d55
Amit Moryossef <amitmoryossef@gmail.com>
2024-10-10 10:15:40 +0200

    fix(concatenate): trim pose, ran multiple times should return the same result

--- spoken_to_signed/gloss_to_pose/concatenate.py
+++ spoken_to_signed/gloss_to_pose/concatenate.py
@@ -11,9 +11,16 @@ def normalize_pose(pose: Pose) -> Pose:
     return pose.normalize(pose_normalization_info(pose.header))
 
 
+def scale_normalized_pose(pose: Pose):
+    new_width = 500
+    shift = 1.25
+    shift_vec = np.full(shape=(pose.body.data.shape[-1]), fill_value=shift, dtype=np.float32)
+    pose.body.data = (pose.body.data + shift_vec) * new_width
+    pose.header.dimensions.height = pose.header.dimensions.width = int(new_width * shift * 2)
+
 def trim_pose(pose, start=True, end=True):
     if len(pose.body.data) == 0:
-        return pose
+        raise ValueError("Cannot trim an empty pose")
 
     wrist_indexes = [
         pose.header._get_point_index('LEFT_HAND_LANDMARKS', 'WRIST'),
@@ -22,7 +29,7 @@ def trim_pose(pose, start=True, end=True):
     either_hand = pose.body.confidence[:, 0, wrist_indexes].sum(axis=1) > 0
 
     first_non_zero_index = np.argmax(either_hand) if start else 0
-    last_non_zero_index = (len(either_hand) - np.argmax(either_hand[::-1]) - 1) if end else len(either_hand)
+    last_non_zero_index = (len(either_hand) - np.argmax(either_hand[::-1])) if end else len(either_hand)
 
     pose.body.data = pose.body.data[first_non_zero_index:last_non_zero_index]
     pose.body.confidence = pose.body.confidence[first_non_zero_index:last_non_zero_index]
@@ -50,10 +57,6 @@ def concatenate_poses(poses: List[Pose]) -> Pose:
 
     # Scale the newly created pose
     print('Scaling pose...')
-    new_width = 500
-    shift = 1.25
-    shift_vec = np.full(shape=(pose.body.data.shape[-1]), fill_value=shift, dtype=np.float32)
-    pose.body.data = (pose.body.data + shift_vec) * new_width
-    pose.header.dimensions.height = pose.header.dimensions.width = int(new_width * shift * 2)
+    scale_normalized_pose(pose)
 
     return pose
