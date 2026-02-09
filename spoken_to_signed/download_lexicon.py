1ba7c9ff437d07e5fc71d7a0de715838c8acaefb
bricksdont <mathias.mueller@uzh.ch>
2026-02-09 15:30:42 +0100

    fix ruff format check

--- spoken_to_signed/download_lexicon.py
+++ spoken_to_signed/download_lexicon.py
@@ -8,13 +8,13 @@ from pose_format.numpy import NumPyPoseBody
 from pose_format.utils.reader import BufferReader
 from tqdm import tqdm
 
-LEXICON_INDEX = ['path', 'spoken_language', 'signed_language', 'start', 'end', 'words', 'glosses', 'priority']
+LEXICON_INDEX = ["path", "spoken_language", "signed_language", "start", "end", "words", "glosses", "priority"]
 
 
 def init_index(index_path: str):
     if not os.path.isfile(index_path):
         # Create csv file with specified header
-        with open(index_path, 'w', encoding='utf-8', newline='') as file:
+        with open(index_path, "w", encoding="utf-8", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(LEXICON_INDEX)
 
@@ -42,19 +42,19 @@ def load_signsuisse(directory_path: str) -> list[dict[str, str]]:
     # for cache busting, we use today's date
     date_str = datetime.now().strftime("%Y-%m-%d")
     config = SignDatasetConfig(name=date_str, version="1.0.0", include_video=False, include_pose="holistic")
-    dataset = tfds.load(name='sign_suisse', builder_kwargs={"config": config})
+    dataset = tfds.load(name="sign_suisse", builder_kwargs={"config": config})
 
     with open(_POSE_HEADERS["holistic"], "rb") as buffer:
         pose_header = PoseHeader.read(BufferReader(buffer.read()))
 
     for datum in tqdm(dataset["train"]):
-        uid_raw = datum['id'].numpy().decode('utf-8')
-        spoken_language = datum['spokenLanguage'].numpy().decode('utf-8')
-        signed_language = iana_tags[datum['signedLanguage'].numpy().decode('utf-8')]
-        words = datum['name'].numpy().decode('utf-8')
+        uid_raw = datum["id"].numpy().decode("utf-8")
+        spoken_language = datum["spokenLanguage"].numpy().decode("utf-8")
+        signed_language = iana_tags[datum["signedLanguage"].numpy().decode("utf-8")]
+        words = datum["name"].numpy().decode("utf-8")
 
         # Load pose and save to file
-        tf_pose = datum['pose']
+        tf_pose = datum["pose"]
         fps = int(tf_pose["fps"].numpy())
         if fps == 0:
             continue
@@ -66,32 +66,33 @@ def load_signsuisse(directory_path: str) -> list[dict[str, str]]:
             pose.write(f)
 
         yield {
-            'path': pose_relative_path,
-            'spoken_language': spoken_language,
-            'signed_language': signed_language,
-            'words': words,
-            'start': "0",
-            'end': str(len(pose_body.data) / fps),  # pose duration
-            'glosses': "",
-            'priority': "",
+            "path": pose_relative_path,
+            "spoken_language": spoken_language,
+            "signed_language": signed_language,
+            "words": words,
+            "start": "0",
+            "end": str(len(pose_body.data) / fps),  # pose duration
+            "glosses": "",
+            "priority": "",
         }
 
 
 def normalize_row(row: dict[str, str]):
-    if row['glosses'] == "" and row['words'] != "":
+    if row["glosses"] == "" and row["words"] != "":
         from spoken_to_signed.text_to_gloss.simple import text_to_gloss
+
         try:
-            sentences = text_to_gloss(text=row['words'], language=row['spoken_language'])
+            sentences = text_to_gloss(text=row["words"], language=row["spoken_language"])
             glosses = [g for sentence in sentences for w, g in sentence]
-            row['glosses'] = " ".join(glosses)
+            row["glosses"] = " ".join(glosses)
         except ValueError as e:
-            if not ('Language' in str(e) and 'not supported' in str(e)):
+            if not ("Language" in str(e) and "not supported" in str(e)):
                 raise e
 
 
 def get_data(name: str, directory: str):
     data_loaders = {
-        'signsuisse': load_signsuisse,
+        "signsuisse": load_signsuisse,
     }
     if name not in data_loaders:
         raise NotImplementedError(f"{name} is unknown.")
@@ -100,11 +101,11 @@ def get_data(name: str, directory: str):
 
 
 def add_data(data: list[dict[str, str]], directory: str):
-    index_path = os.path.join(directory, 'index.csv')
+    index_path = os.path.join(directory, "index.csv")
     os.makedirs(directory, exist_ok=True)
     init_index(index_path)
 
-    with open(index_path, 'a', encoding='utf-8', newline='') as file:
+    with open(index_path, "a", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         for row in tqdm(data):
             normalize_row(row)
@@ -115,7 +116,7 @@ def add_data(data: list[dict[str, str]], directory: str):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--name", choices=['signsuisse'], required=True)
+    parser.add_argument("--name", choices=["signsuisse"], required=True)
     parser.add_argument("--directory", type=str, required=True)
     args = parser.parse_args()
 
@@ -123,5 +124,5 @@ def main():
     add_data(data, args.directory)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
