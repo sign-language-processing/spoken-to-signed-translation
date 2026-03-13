0bfca7926448daefc3c94d543c26b03829e0f3a9
Gerard Sant <gerard.sant.muniesa@gmail.com>
2026-03-13 12:53:37 +0100

    [FIX] Fix ruff lint errors in bin.py

--- spoken_to_signed/bin.py
+++ spoken_to_signed/bin.py
@@ -212,7 +212,8 @@ def text_to_gloss_to_pose_to_video():
 
     sentences = _text_to_gloss(args.text, args.spoken_language, args.glosser, signed_language=args.signed_language)
     result = _gloss_to_pose(
-        sentences, args.lexicon, args.spoken_language, args.signed_language, disable_fingerspelling=args.disable_fingerspelling
+        sentences, args.lexicon, args.spoken_language, args.signed_language,
+        disable_fingerspelling=args.disable_fingerspelling
     )
     _pose_to_video(result.pose, args.video)
 
